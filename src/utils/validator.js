4d7373d61843f3897832d6c39f3844e223e0e5f4
SariqueZamal <sariquezamal0@gmail.com>
2022-05-24 11:54:02 +0530

    update validator.

---
+++ src/utils/validator.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose")
+
+const isValidData = function (value) {
+    if (typeof value === "undefined" || value === null) return false;
+    if (typeof value === "string" && value.trim().length === 0) return false;
+    return true;
+}
+
+const isValidRequestBody = function (requestBody) {
+    return Object.keys(requestBody).length > 0;
+};
+
+
+
+
+
+module.exports = { isValidData, isValidRequestBody}
