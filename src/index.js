18594799c3f9b44d3ad9be750f133d78ad5ddf20
Rishav Kumar <rishavkr000@gmail.com>
2022-08-01 20:53:08 +0530

    .

--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
-const express = require('express')
-const multer = require("multer")
-const { AppConfig } = require('aws-sdk')
-const bodyparser = require('body-parser')
-const mongoose = require('mongoose')
+const express = require("express");
+const multer = require("multer");
+const { AppConfig } = require("aws-sdk");
+const router = require("./routes/route");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const port = 4000;
 
-const app = express()
-app.use(bodyparser.json())
-app.use(multer().any())
+const app = express();
+app.use(bodyParser.json());
+app.use(multer().any());
 
-const router = require('./routes/route')
+mongoose
+  .connect(
+    "mongodb+srv://group15_project:EDHBqxqKYJaki5EJ@cluster0.i9alz.mongodb.net/Rishav5th",
+    {
+      useNewUrlParser: true,
+    }
+  )
+  .then(() => console.log("mongoDB is Connected!!"))
+  .catch((err) => console.log(err));
 
-mongoose.connect("mongodb+srv://group15_project:EDHBqxqKYJaki5EJ@cluster0.i9alz.mongodb.net/Rishav5th",
-    { useNewUrlParser: true })
-    .then(() => console.log("mongoDB is Connected!!"))
-    .catch(err => console.log(err))
+app.use("/", router);
 
-app.use('/', router)
-
-app.listen(process.env.PORT || 3000, () => {
-    console.log("server connected at Port :", process.env.PORT || 3000)
-})
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
