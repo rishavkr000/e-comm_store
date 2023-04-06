9ef8a8cec2c126a2a609fb3b1d1519da34c8a7b3
Rishav Kumar <rishavkr000@gmail.com>
2023-04-06 18:19:09 +0530

    .

--- src/routes/route.js
+++ src/routes/route.js
@@ -20,7 +20,6 @@ router.get('/products', getProduct)
 router.get('/products/:productId', getProductById)
 router.put('/products/:productId', updateProduct)
 router.delete('/products/:productId', deleteProductById)
-router.patch('/products/:productId', deleteProductById)
 
 
 // 3. Cart
